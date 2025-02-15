43ad8ad30eecaef32099cf929ad0b71f3fb617ea
HMarchiori <hmarchiori2003@gmail.com>
2025-02-14 21:49:08 -0300

    BUILD: update to section 11

--- src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -1,10 +1,11 @@
-INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
-	(1, 'São Paulo - Brasil', 'Ayrton', 'Male', 'Senna'),
-	(2, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
-	(4, 'Porbandar - India', 'Indira', 'Female', 'Gandhi'),
-	(5, 'Porbandar - India', 'Mahatma', 'Male', 'Gandhi'),
-	(7, 'Kentucky - US', 'Muhamamd', 'Male', 'Ali'),
-	(9, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
-	(10, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
-	(11, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla'),
-	(12, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`, `birth_day`)
+VALUES
+    (1, 'São Paulo - Brasil', 'Ayrton', 'Male', 'Senna', '1960-03-21'),
+    (2, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci', '1952-04-15'),
+    (4, 'Porbandar - India', 'Indira', 'Female', 'Gandhi', '1917-11-19'),
+    (5, 'Porbandar - India', 'Mahatma', 'Male', 'Gandhi', '1869-10-02'),
+    (7, 'Kentucky - US', 'Muhammad', 'Male', 'Ali', '1942-01-17'),
+    (9, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela', '1918-07-18'),
+    (10, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela', '1918-07-18'),
+    (11, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla', '1956-07-10'),
+    (12, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla', '1936-07-10');
